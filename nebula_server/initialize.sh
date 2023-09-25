d90bd5606063413b665f169e4400e4db261dd4ee
salvatore <salvatoreindino00@gmail.com>
2023-09-25 18:10:23 +0200

    Sistemazione script avvio automatico dei servizi, in particolare avvio automatico dockerd

--- nebula_server/initialize.sh
+++ nebula_server/initialize.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
+echo "Avvio il servizio dockerd"
+env
 
-#echo "Stampa informazioni sul percorso"
-#echo "Percorso attuale: $PWD"
-#echo "Percorso di Docker: $(which dockerd)"
-#echo "Percorso di k3d: $(which k3d)"
-#echo "Percorso di kubectl: $(which kubectl)"
-#echo "nameserver 8.8.8.8" > /etc/resolv.conf
-#echo "nameserver 8.8.4.4" > /etc/resolv.conf
-
-# Avvia il processo dockerd in background e disconnettilo dal terminale
-echo "avvio il servizio dockerd"
-sleep 10
- #dockerd >/dev/null 2>&1 &
- dockerd >/dev/null 2>&1 &
-#exec dockerd
-
-#bloccare e verificare
 read
-# Avvia il cluster Kubernetes utilizzando k3d in background
-echo "avvio cluster comp-cluster"
-sleep 60 #aspetto che dockerd si avvii correttamente
-  k3d cluster create comp-cluster #>/dev/null 2>&1 &
-  #k3d cluster list
+export DOCKER_HOST="unix:///var/run/docker.sock"
+dockerd &
+echo "PREMI un Tasto"
+read
+echo "STAMPO INFO  servizio dockerd"
+docker info
+echo "PREMI un Tasto per CHIUDERE"
+read
+# Controlla che dockerd sia in esecuzione
+#while ! docker info >/dev/null 2>&1; do
 
-
-echo "avvio pod comp.yaml"
-sleep 10
- kubectl apply -f comp.yaml #>/dev/null 2>&1 &
-
-# Attendi che il cluster Kubernetes sia pronto (puoi aggiungere un'attesa qui, se necessario)
-
-# Esegui Nebula con il file di configurazione specificatoexec
-echo "avvio il servizio nebula"
-sleep 10
- ./nebula -config /nebula/config.yaml
+ #   sleep 1
+#done
 
 
