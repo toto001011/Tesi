735abe548807d79bf232919869f15a782ff51495
toto001011 <tu@esempio.com>
2024-03-01 17:58:15 +0100

    implementazione servizio k3s, server attivo su host1, utilizzando un immagine di un progetto github k3s-dind

---
+++ host1/start_services.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Avvio lo script di star k3s"
+start-k3s.sh >/dev/null 2>&1 & 
+
+echo "Configure la variabile DOCKER_HOST"
+
+  
+
+
+# Controlla che dockerd sia in esecuzione
+while ! docker info >/dev/null 2>&1; do
+    echo "In attesa del completamento di dockerd..."
+    sleep 1
+done
+
+echo "Il servizio dockerd è stato avviato correttamente."
+
+echo "Aggiungo regola di routing"
+
+
+ip route add 10.9.0.0/24 via 192.168.60.254
+#ip route add 192.198.0.0/24 via 10.0.9.254
+echo "Avvio il servizio Nebula"
+./nebula -config /nebula/config.yaml
+
